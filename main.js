6eaba20472c3222b891e140c034acd3406a7703f
Nick-Web <95388164+elcrck@users.noreply.github.com>
2022-03-23 04:38:34 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+(function (){
+
+  let btnYes = document.getElementById("btn_yes");
+  let btnNo = document.getElementById("btn_no");
+
+  btnYes.addEventListener("click", () => {
+    alert("Gracias por darme una oportunidad")
+  })
+
+  btnNo.addEventListener("mouseover", () => {
+    let y = Math.round(Math.random() * 80);
+    let x = Math.round(Math.random() * 80);
+
+    btnNo.style.top = y + "%";
+    btnNo.style.left = x + "%";
+  })
+
+})()
